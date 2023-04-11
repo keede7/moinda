ab345a0d7fe4848894b2d5c8a200d2bc7e872817
Yoonhwan Kim <wesaq11@gmail.com>
2023-04-11 16:22:46 +0900

    [#20] feat: Querydsl QDomain Create Setting (#10)
    Co-authored-by: yoonhwan <yoonhwan.kim@kbhc.co.kr>

--- moinda-core/src/main/kotlin/io/keede/moinda/core/config/JpaConfig.kt
+++ moinda-core/src/main/kotlin/io/keede/moinda/core/config/JpaConfig.kt
@@ -1,12 +1,24 @@
 package io.keede.moinda.core.config
 
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
 
 @Configuration
 @EnableJpaAuditing
 @EnableJpaRepositories(
     basePackages = ["io.keede.moinda"]
 )
-class JpaConfig
+class JpaConfig(
+    @PersistenceContext
+    private val entityManager: EntityManager
+) {
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(this.entityManager)
+    }
+}
