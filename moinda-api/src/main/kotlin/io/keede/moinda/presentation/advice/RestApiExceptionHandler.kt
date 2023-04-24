e5a16f4609c83223072de44b9ebca00ac03b0ce7
Yoonhwan Kim <wesaq11@gmail.com>
2023-04-24 16:27:34 +0900

    [#53] feat: Add Rest Api Handling Code (#34)

---
+++ moinda-api/src/main/kotlin/io/keede/moinda/presentation/advice/RestApiExceptionHandler.kt
@@ -0,0 +1,31 @@
+package io.keede.moinda.presentation.advice
+
+import org.springframework.http.HttpStatus
+import org.springframework.validation.BindException
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class RestApiExceptionHandler {
+
+    @ExceptionHandler(value = [RuntimeException::class])
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleRuntimeException(exception: RuntimeException): String? {
+        println("exception !!! : ${exception.message}")
+        return exception.message
+    }
+
+    // TODO : 추가적인 테스트가 필요하다.
+    @ExceptionHandler(value = [BindException::class, MethodArgumentNotValidException::class])
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleRequestArgumentBindingException(bindingResult: BindingResult): String? {
+        return bindingResult.fieldErrors
+            .map { obj: FieldError -> obj.defaultMessage }
+            .toList()
+            .joinToString()
+    }
+}
