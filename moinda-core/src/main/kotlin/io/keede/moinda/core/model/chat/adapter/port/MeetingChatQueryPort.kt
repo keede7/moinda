4001d946a6d45b92890e031ab343063692558394
Yoonhwan Kim <wesaq11@gmail.com>
2023-05-14 15:38:01 +0900

    [#98] feat: add to MeetingChat Query API & move To WebSocket Package

--- moinda-core/src/main/kotlin/io/keede/moinda/core/model/chat/adapter/port/MeetingChatQueryPort.kt
+++ moinda-core/src/main/kotlin/io/keede/moinda/core/model/chat/adapter/port/MeetingChatQueryPort.kt
@@ -2,8 +2,10 @@ package io.keede.moinda.core.model.chat.adapter.port
 
 import com.querydsl.jpa.impl.JPAQueryFactory
 import io.keede.moinda.core.model.chat.adapter.MeetingChatQueryAdapter
-import io.keede.moinda.core.model.chat.entity.MeetingChatJpaEntity
+import io.keede.moinda.core.model.chat.entity.MeetingChatProjection
 import io.keede.moinda.core.model.chat.entity.QMeetingChatJpaEntity
+import io.keede.moinda.core.model.chat.entity.QMeetingChatJpaEntity.meetingChatJpaEntity
+import io.keede.moinda.core.model.chat.entity.QMeetingChatProjection
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
 /**
@@ -14,8 +16,22 @@ internal class MeetingChatQueryPort(
     private val jpaQueryFactory: JPAQueryFactory
 ) : MeetingChatQueryAdapter, QuerydslRepositorySupport(QMeetingChatJpaEntity::class.java) {
 
-    override fun findChatsByMeetingId(meetingId: Long): List<MeetingChatJpaEntity> {
-        TODO("Not yet implemented")
+    // TODO : 메세지들을 보여줄 때의 설계가 추가적으로 필요함, EX) 스크롤링으로 추가요청 , 캐시전략 사용, 페이징처리
+    override fun findChatsByMeetingId(meetingId: Long): List<MeetingChatProjection> {
+        return jpaQueryFactory.select(
+            QMeetingChatProjection(
+                meetingChatJpaEntity.id,
+                meetingChatJpaEntity.memberJpaEntity.name,
+                meetingChatJpaEntity.context,
+                meetingChatJpaEntity.writeAt.stringValue()
+            )
+        )
+            .from(meetingChatJpaEntity)
+            .where(
+                meetingChatJpaEntity.meetingJpaEntity.id.eq(meetingId)
+                    .and(meetingChatJpaEntity.deleteStatus.isFalse)
+            )
+            .fetch()
     }
 
 }
