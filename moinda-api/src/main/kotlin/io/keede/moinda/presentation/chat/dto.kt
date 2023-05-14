1a6326724b66a7980ecdb4e66745b6b54c7fb2cf
Yoonhwan Kim <wesaq11@gmail.com>
2023-05-14 14:25:38 +0900

    [#97] fix: fix to STMOP Session Error

--- moinda-api/src/main/kotlin/io/keede/moinda/presentation/chat/dto.kt
+++ moinda-api/src/main/kotlin/io/keede/moinda/presentation/chat/dto.kt
@@ -4,19 +4,32 @@ import com.fasterxml.jackson.annotation.JsonFormat
 import io.keede.moinda.common.chat.CreateMeetingChat
 import io.keede.moinda.domains.chat.domain.MeetingChat
 import java.time.LocalDateTime
+import javax.validation.constraints.NotEmpty
 
 /**
  * @author keede
  * Created on 2023-05-10
  */
-
 data class CreateMeetingChatDto(
+    // TODO : Validation 설정 필요.
     val context: String,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", locale = "Asia/Seoul")
     val writeAt: LocalDateTime,
     val meetingId: Long,
     val memberId: Long,
 )
+
+// 기존에 설계된 변수명에 맞춰서 사용.
+data class MeetingChatEventSubscriber(
+    val writer: String?,
+    val status: MessageType,
+)
+
+enum class MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
 
 data class MeetingChatResponseDto(
     val chattingId: Long?,
